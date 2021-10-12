f95e8830fd6bf956a67b8f5d172ce4954110bd2e
Balázs Krisztián <61137345+TurboCsiga@users.noreply.github.com>
2021-10-12 17:32:46 +0200

    js

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 function general(){
-    document.getElementById("lista").innerHTML = "";
-
-    let lista = document.getElementById("beText").value;
-    let li = document.createElement("li");
-    input.type = "checkbox";
-    
-    lista.appendChild(li);
-    li.appendChild(beText);
-    document.getElementById("lista").appendChild(li);
+    let beText = document.getElementById("beText");
+    let ujSor = document.createElement("div");
+    ujSor.className = "sor";
+    let Box = document.createElement("beText");
+    Box.type = "checkbox";
+    ujSor.appendChild(Box);
+    let szoveg = document.createElement("label");
+    szoveg.innerHTML = beText.value;
+    ujSor.appendChild(szoveg);
+    let x = document.createElement("label");
+    x.innerHTML = "X";
+    x.className = "X";
+    ujSor.appendChild(x);
+    document.getElementById("lista").appendChild(ujSor);
+    beText.value = "";
 }
 
 function init(){
     document.getElementById("hozzaAd").addEventListener("click", general);
-    document.getElementById("beText").addEventListener("input", general);
 }
 document.addEventListener("DOMContentLoaded", init);
