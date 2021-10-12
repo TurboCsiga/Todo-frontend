bccad432f1fa8057a9b4fe7ec73a945974a0c6c1
Balázs Krisztián <61137345+TurboCsiga@users.noreply.github.com>
2021-10-12 16:46:22 +0200

    .

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ function general(){
     document.getElementById("lista").innerHTML = "";
 
     let lista = document.getElementById("beText").value;
-    let table = document.createElement("table");
     let li = document.createElement("li");
     input.type = "checkbox";
-
-    table.appendChild(li);
+    
+    lista.appendChild(li);
     li.appendChild(beText);
     document.getElementById("lista").appendChild(li);
 }
 
 function init(){
     document.getElementById("hozzaAd").addEventListener("click", general);
+    document.getElementById("beText").addEventListener("input", general);
 }
 document.addEventListener("DOMContentLoaded", init);
