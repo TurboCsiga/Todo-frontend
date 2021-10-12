64c737abee64ab7e895cc7e08499495b27af471e
Balázs Krisztián <61137345+TurboCsiga@users.noreply.github.com>
2021-10-12 16:39:14 +0200

    izé

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function general(){
     document.getElementById("lista").innerHTML = "";
 
-    let lista = document.getElementById("lista").value;
+    let lista = document.getElementById("beText").value;
     let table = document.createElement("table");
     let li = document.createElement("li");
+    input.type = "checkbox";
+
     table.appendChild(li);
+    li.appendChild(beText);
+    document.getElementById("lista").appendChild(li);
 }
 
 function init(){
